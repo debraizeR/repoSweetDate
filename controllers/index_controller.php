976beb89c8e9e272a95fbcecf8f20d64702f36d2
Raphael Debraize <raphaeldebraize@gmail.com>
2021-01-27 15:35:28 +0100

    début regex cookie

--- controllers/index_controller.php
+++ controllers/index_controller.php
@@ -15,7 +15,7 @@ else
         setcookie("cookieLastname", $_POST["lastname"], time()+3600*24, "/");
         setcookie("cookieAge", $_POST["age"], time()+3600*24, "/");
         setcookie("cookieGender", $_POST["gender"], time()+3600*24, "/");
-        setcookie("cookieMail", $_POST["mail"], time()+3600*24, "/");
+        regexMail($_POST["mail"]);
         setcookie("cookieZipcode", $_POST["zipcode"], time()+3600*24, "/");
         setcookie("cookieChoice",$_POST["choice"], time()+3600*24, "/");
         header("location: index.php");
@@ -25,6 +25,23 @@ else
     
 }
 
+function regexFirstName($firstname)
+{
+    $regex = "/aerzgetr/i";
+}
+
+function regexMail($testMail)
+{
+    $regex = "/^[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,4}$/";
+    if(preg_match($regex, $testMail))
+    {
+        setcookie("cookieMail", $testMail, time()+3600*24, "/");
+    }
+    else
+    {
+        setcookie("cookieMail", "echec", time()+3600*24, "/");
+    }
+}
 
 
 ?>
