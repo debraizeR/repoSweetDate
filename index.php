3e74bea09eaad5c430c0def07a044d573d6e8f28
zaharat <zaharat_b@hotmail.fr>
2021-01-28 16:53:45 +0100

    modif user

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ if ($testCookie) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sweet Date</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
   <link rel="stylesheet" href="/assets/css/style.css" />
   <link rel="shortcut icon" type="image/x-icon" href="/assets/img/logo3.png">
 
@@ -43,8 +45,8 @@ if ($testCookie) {
             <div class="card-body cardindex">
               <form action="index.php" method="post">
 
-                <a href="#" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal" id="Homme" name="choice" value="Homme">Vous recherchez un homme</a>
-                <a href="#" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal" id="Femme" name="choice" value="Femme">Vous recherchez une femme</a>
+                <a href="#" class="animate__shakeX btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal" id="Homme" name="choice" value="Homme">Vous recherchez un homme</a>
+                <a href="#" class="animate__shakeX btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal" id="Femme" name="choice" value="Femme">Vous recherchez une femme</a>
 
                 <!-- Modal -->
                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -77,8 +79,8 @@ if ($testCookie) {
                       </div>
                       <div class="modal-footer">
                         <input type="hidden" name="choice" id="choice">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" id="confirm" name="confirm" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                        <button type="submit" id="confirm" name="confirm" class="btn btn-primary">Sauvegarder</button>
                       </div>
 
                     </div>
@@ -99,18 +101,11 @@ if ($testCookie) {
 
 Reposant sur le même test de personnalité, scientifique complet que celui utilisé par eDarling, les membres sont mis en relation en fonction de leurs exigences. L’efficacité de ce test de personnalité et de l’algorithme de compatibilité s’appuie sur le modèle des Big Five Personnality Traits. Ce procédé permet de déterminer la compatibilité des couples et de les mettre en relation avec les personnes qui leur correspondent le mieux.
 
-EliteRencontre est un site pour célibataires privilégiés, aussi ambitieux dans leur vie personnelle que dans leur vie professionnelle et qui souhaitent rencontrer des personnes ayant le même statut social qu’eux.
+Sweet Dat est un site pour célibataires privilégiés, aussi ambitieux dans leur vie personnelle que dans leur vie professionnelle et qui souhaitent rencontrer des personnes ayant le même statut social qu’eux.
 
 EliteRencontre souhaite offrir un service haut de gamme à ses membres et leur permettre de rencontrer LA personne qui les comble vraiment.
 
-Notre algorithme de compatibilité
-L’algorithme de compatibilité EliteRencontre est fondé sur un modèle mathématique qui permet de déterminer la compatibilité des membres par rapport à des données sociodémographiques, des préférences personnelles, et des données psychologiques. Les détails que donnent nos utilisateurs dans leurs réponses à notre questionnaire – le plus complet sur le marché, constituent la base de calcul pour ces trois domaines. Les données démographiques regroupent des données basiques importantes comme le genre, l’âge, le lieu recherché ou le niveau d’études.
 
-Les préférences personnelles comprennent des questions sur certains aspects de la vie des membres qui peuvent se révéler essentiels pour la stabilité et l’harmonie d’une relation. Par exemple, notre questionnaire comprend des questions comme « Quelle importance accordez-vous à la religion de la personne que vous recherchez ? » ou encore « Souhaitez-vous avoir des enfants ? ».
-
-L’analyse psychologique d’EliteRencontre est basée sur le modèle des « Big Five Personality Traits » (en français : les cinq traits de personnalité principaux), un modèle qui, dans les années 1990, a été l’un des plus influents et des plus connus de la psychologie moderne. En fonction des caractéristiques et des attentes communiquées par les utilisateurs, il est possible d’identifier deux personnes ayant de grandes chances de former une relation durable et épanouie. Si leurs données démographiques ainsi que leurs préférences présentent une compatibilité élevée, les profils seront alors présentés l’un à l’autre.
-
-L’un des points cruciaux auquel nous apportons une très grande importante est la sécurité des données personnelles. Toutes les données sont reçues et stockées sous forme codée et ne sont jamais transmises à des tiers, ni même rendues visibles aux autres utilisateurs.
               </p>
             </div>
           </div>
--- views/user.php
+++ views/user.php
@@ -1,5 +1,9 @@
 <?php require("../controllers/user_controller.php");
-if(isset($_POST["deleteCookie"])){deleteAllCookie(); header("Location: ../index.php");exit();}
+if (isset($_POST["deleteCookie"])) {
+    deleteAllCookie();
+    header("Location: ../index.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,49 +13,56 @@ if(isset($_POST["deleteCookie"])){deleteAllCookie(); header("Location: ../index.
     <title>Lovers</title>
     <meta charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/style.css"/>
+    <link rel="stylesheet" href="../assets/css/style.css" />
 
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light-dark">
-    <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light-dark">
+        <div class="container-fluid">
 
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="../index.php">Accueil</a>
-        <a class="nav-link active" aria-current="page" href="../views/lovers.php">Nos célibataires</a>
-        <a class="nav-link" href="#"></a>
-        <a type="button" class="btn1 btn connexion" href="">Profil</a></button>
-      </div>
-    </div>
-    </div>
-  </nav>
-<div class="container">
-    <div class="row">
-        <div class="col-8">
-            <p>Prénom : <?= $firstname; ?></p>
-            <p>Nom : <?= $lastname; ?></p>
-            <p>Age : <?= $age; ?></p>
-            <p>Genre : <?= $gender; ?></p>
-            <p>Adresse mail : <?= $mail; ?></p>
-            <p>Code postal : <?= $zipcode; ?></p>
-            <p>Genre recherché : <?= $choice; ?></p>
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="../index.php">Accueil</a>
+                <a class="nav-link active" aria-current="page" href="../views/lovers.php">Nos célibataires</a>
+                <a class="nav-link" href="#"></a>
+                <a type="button" class="btn1 btn connexion" href="">Profil</a></button>
+            </div>
         </div>
-        <div class="col-4">
-            <div class="row justify-content-center">
+    </nav>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div class="card card1">
+                <div class="card-header">
+                    Votre profil
+                </div>
+                <div class="card-body">
+
+                    <h5 class="card-text">Nom : <?= $firstname; ?></>
+                        <p>Prénom : <?= $lastname; ?></p>
+                        <p>Age : <?= $age; ?></p>
+                        <p>Genre : <?= $gender; ?></p>
+                        <p>Adresse mail : <?= $mail; ?></p>
+                        <p>Code postal : <?= $zipcode; ?></p>
+                        <p>Genre recherché : <?= $choice; ?></p <button><a href="#" class="btnprofil btn-primary">Modifier votre profil</a></button>
+                </div>
+            </div>
+        </div>
+
+
+
+            <div class="col-4">
                 <form action="user.php" method="post">
                     <button type="submit" class="btn justify-content-center" id="deleteCookie" name="deleteCookie">Supprimer les cookies</button>
                 </form>
-            </div>
-            <div class="row justify-content-center">
+
                 <a type="button" class="btn justify-content-center" id="meetc" name="meetic" href="https://www.meetic.fr/">TAKE MY MONEY</a>
-                    
             </div>
         </div>
     </div>
-    
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>   
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 
 </html>
